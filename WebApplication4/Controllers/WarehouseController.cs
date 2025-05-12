7f1ef4e3a25d180cf7c1f7ae8989a6ff97eb47ca
Twoje Imi i Nazwisko <Twj email powizany z GitHubem>
2025-05-12 23:33:44 +0200

    controller method skeleton

--- WebApplication4/Controllers/WarehouseController.cs
+++ WebApplication4/Controllers/WarehouseController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.contracts.requests;
+using WebApplication4.entities;
 
 namespace WebApplication4.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class WarehouseController: ControllerBase
 {
+    [HttpPost("[action]")]
+    public async Task<IActionResult> AddWarehouse([FromBody] CreateProductWarehouse warehouse,CancellationToken token = default)
+    {
+        return Ok(warehouse);
+    }
     
 }
